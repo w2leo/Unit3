433d85c918f895026d5dd82211a07657630f58ee
w2leo <82013108+w2leo@users.noreply.github.com>
2022-10-18 16:21:35 +0300

    Stop spawn Obstacles
    lookup fo GameIsOver property in PlayerController

--- Assets/Scripts/SpawnManager.cs
+++ Assets/Scripts/SpawnManager.cs
@@ -8,15 +8,19 @@ public class SpawnManager : MonoBehaviour
     private Vector3 spawnPosition = new Vector3(25, 0, 0);
     private float startDelayInSec = 2f;
     private float repeatRateInSec = 2f;
+    private PlayerController playerController;
 
     private void Start()
     {
         InvokeRepeating("SpawnObstacle", startDelayInSec, repeatRateInSec);
+        playerController = FindObjectOfType<PlayerController>();
     }
 
     private void SpawnObstacle()
     {
-        Instantiate(obstaclePrefab, spawnPosition, obstaclePrefab.transform.rotation);
+        if (!playerController.GameIsOver)
+        {
+            Instantiate(obstaclePrefab, spawnPosition, obstaclePrefab.transform.rotation);
+        }
     }
-
 }
