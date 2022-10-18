a81bd1b4e4294082d7d4fcfb4b503412d10ae99e
w2leo <82013108+w2leo@users.noreply.github.com>
2022-10-18 11:26:03 +0300

    Update SpawnManager
    Change variable names

--- Assets/Scripts/SpawnManager.cs
+++ Assets/Scripts/SpawnManager.cs
@@ -6,12 +6,12 @@ public class SpawnManager : MonoBehaviour
 {
     [SerializeField] private GameObject obstaclePrefab;
     private Vector3 spawnPosition = new Vector3(40, 0, 0);
-    private float startDelay = 2f;
-    private float repeatRate = 2f;
+    private float startDelayInSec = 2f;
+    private float repeatRateInSec = 2f;
 
     private void Start()
     {
-        InvokeRepeating("SpawnObstacle", startDelay, repeatRate);
+        InvokeRepeating("SpawnObstacle", startDelayInSec, repeatRateInSec);
     }
 
     private void SpawnObstacle()
