a50f6d9ab655170613613873e1463ea77a12dccc
w2leo <82013108+w2leo@users.noreply.github.com>
2022-10-18 09:56:20 +0300

    Jump player added

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -2,17 +2,22 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(Rigidbody))]
 public class PlayerController : MonoBehaviour
 {
-    // Start is called before the first frame update
+    private Rigidbody playerRb;
+
     void Start()
     {
-        
+        playerRb = GetComponent<Rigidbody>();
+        playerRb.AddForce(Vector3.up * 300f);
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            playerRb.AddForce(Vector3.up * 10f, ForceMode.Impulse);
+        }
     }
 }
