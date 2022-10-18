c981b137e047afc5a49bd1acde20e48fb839c064
w2leo <82013108+w2leo@users.noreply.github.com>
2022-10-18 11:25:13 +0300

    Update SpawnManager
    added multi spawns in repeatRate in seconds.

--- Assets/Scripts/SpawnManager.cs
+++ Assets/Scripts/SpawnManager.cs
@@ -6,8 +6,15 @@ public class SpawnManager : MonoBehaviour
 {
     [SerializeField] private GameObject obstaclePrefab;
     private Vector3 spawnPosition = new Vector3(40, 0, 0);
+    private float startDelay = 2f;
+    private float repeatRate = 2f;
 
-    void Start()
+    private void Start()
+    {
+        InvokeRepeating("SpawnObstacle", startDelay, repeatRate);
+    }
+
+    private void SpawnObstacle()
     {
         Instantiate(obstaclePrefab, spawnPosition, obstaclePrefab.transform.rotation);
     }
